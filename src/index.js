eadde85a5394cd79f5cd52e9e2ac89b7c137826e
stricovtsov <strik85@tut.by>
2023-01-27 23:40:55 +0300

    feat:add my solution1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+   // your solution
+   const openBrackets = [];
+   for (i=0; i<bracketsConfig.length; i++) {
+     openBrackets.push(bracketsConfig[i][0]);
+   }
+   const closeBrackets = []
+   for (i=0; i<bracketsConfig.length; i++) {
+     closeBrackets.push(bracketsConfig[i][1]);
+   }
+   const bracketsPair = {};
+   for (i=0; i<bracketsConfig.length; i++) {
+     let key = bracketsConfig[i][1];
+     bracketsPair[key] = bracketsConfig[i][0];
+   }
+   const stack = [];
+   for (i=0; i<str.length; i++) {
+     let symbol = str[i];
+     if (openBrackets.includes(symbol)) {
+       if (closeBrackets.includes(symbol) && stack[stack.length-1] === symbol) {
+         stack.pop();
+       } else {
+       stack.push(symbol);
+       }
+     } else {
+       if (stack.length === 0) {
+         return false;
+       }
+       let topSymbol = stack[stack.length-1];
+       if (bracketsPair[symbol] === topSymbol) {
+         stack.pop();
+       } else {
+         return false;
+       }
+     }
+   }
+   return stack.length === 0;
+ }
